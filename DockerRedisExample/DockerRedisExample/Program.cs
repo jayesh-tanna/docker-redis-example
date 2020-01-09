16441c9d21b66fcbc99e91500a0de8a08ef25f26
Jayesh Tanna <tanna.jay90@gmail.com>
2020-01-09 12:41:49 +0530

    Added solution and required classes to implement redis

---
+++ DockerRedisExample/DockerRedisExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using DockerRedisExample.Domains;
+using DockerRedisExample.Redis;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace DockerRedisExample
+{
+    class Program
+    {
+        private const string ProductCacheKey = "AllProducts";
+
+        static void Main(string[] args)
+        {
+            SaveDataToRedis();
+
+            ReadDataFromRedis();
+        }
+
+        public static void ReadDataFromRedis()
+        {
+            var cache = RedisHelper.Connection.GetDatabase();
+
+            var serializedObject = cache.StringGet(ProductCacheKey);
+
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(serializedObject);
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Id + " " + product.Name + " " + product.Price);
+            }
+        }
+
+        public static void SaveDataToRedis()
+        {
+            var cache = RedisHelper.Connection.GetDatabase();
+
+            var service = new ProductService();
+
+            var products = service.GetProducts();
+
+            var serializedObject = JsonConvert.SerializeObject(products);
+
+            cache.StringSet(ProductCacheKey, serializedObject);
+        }
+    }
+}
---
+++ DockerRedisExample/DockerRedisExample/Redis/RedisHelper.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace DockerRedisExample.Redis
+{
+    public class RedisHelper
+    {
+        static RedisHelper()
+        {
+            _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("localhost:6379");
+            });
+        }
+
+        private static readonly Lazy<ConnectionMultiplexer> _lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return _lazyConnection.Value;
+            }
+        }
+    }
+}
